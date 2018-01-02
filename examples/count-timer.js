e1fd8462e9e1767d923be3d31b42fc1f2d2f95b7
Venkataraman Rajasekaran <rvenkataraman@athenahealth.com>
2018-01-02 13:35:16 +0530

    Fixed deprecated function in count-timer

--- examples/count-timer.js
+++ examples/count-timer.js
@@ -1,8 +1,6 @@
-var util = require('util');
-
 var count = 0;
 
 var id = setInterval(function () {
-  util.puts('Count is ' + count + '. Incrementing now.');
+  console.log('Count is ' + count + '. Incrementing now.');
   count++;
 }, 1000);
